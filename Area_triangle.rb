6e4ea945d88ef8b062e59ab8c0166b566042158c
Ilia <ilia.gredinari@gmail.com>
2021-04-01 15:33:24 +0300

    Area triangle

---
+++ Area_triangle.rb
@@ -0,0 +1,11 @@
+puts "Check  Area of a triangle!"
+puts "Put please base number"
+user_base_number = gets.chomp.to_f
+
+puts "Put please height number"
+
+user_height_number = gets.chomp.to_f
+
+sum_triangle = (0.5*user_base_number*user_height_number)
+
+puts "Area of a triangle = #{sum_triangle}"
